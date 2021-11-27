22c51c94444d0887b48916dd3624c8fde6f3cd96
iNViTiON <13241662+iNViTiON@users.noreply.github.com>
2021-11-27 12:21:06 +0700

    chore: add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'storypointer-794f2',
+    appId: '1:271936864591:web:85cc899f9c489b5009c53a',
+    databaseURL: 'https://storypointer-794f2-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'storypointer-794f2.appspot.com',
+    apiKey: 'AIzaSyClKsRibmtXjFOXnLH4tzHiyf2KSSo58II',
+    authDomain: 'storypointer-794f2.firebaseapp.com',
+    messagingSenderId: '271936864591',
+  },
   production: false
 };
 
